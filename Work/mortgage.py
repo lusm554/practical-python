6bbdf94f3a022eb81fe6d9c07ab330987957ca27
lusm <loveyousomuch554@gmail.com>
2023-07-20 21:25:26 +0300

    Exercise 1.9: Making an Extra Payment Calculator

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -8,12 +8,22 @@ payment = 2684.11
 total_paid = 0.0
 month_cnt = 0
 
+# How much will Dave pay if he pays an extra $1000/month for 4 years starting after the first five years have already been paid?
+extra_payment_start_month = 5 * 12
+extra_payment_end_month = (5 + 4) * 12
+extra_payment = 1000
+
 while principal > 0:
 	principal = principal * (1+rate/12) - payment
 	total_paid = total_paid + payment
-	if month_cnt < 12:
-		total_paid += 1000
-		principal -= 1000
+	if month_cnt >= extra_payment_start_month and month_cnt < extra_payment_end_month:
+		total_paid += extra_payment
+		principal -= extra_payment
 	month_cnt += 1
 
 print('Total paid', total_paid)
+
+
+
+# Total paid 929965.6199999959
+
