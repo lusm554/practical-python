0cf6e311b136568f2b7d5ccd532b43610672d196
lusm <loveyousomuch554@gmail.com>
2023-07-21 11:59:56 +0300

    Exercise 1.27: Reading a data file

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,16 @@
 # pcost.py
 #
-# Exercise 1.27
+# Exercise 1.2
+
+filepath = 'Data/portfolio.csv'
+total_cost = 0.0
+
+with open(filepath, 'rt') as f:
+  headers = next(f).strip().split(',')
+  print(headers)
+  for line in f:
+    row = line.strip().split(',')
+    print(row)
+    total_cost += int(row[1]) * float(row[2])
+
+print(f"{total_cost=}")
