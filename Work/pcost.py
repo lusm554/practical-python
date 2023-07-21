37715b46867ee05fc391b2cb4995493283de3fc5
lusm <loveyousomuch554@gmail.com>
2023-07-21 14:58:24 +0300

    Exercise 1.32: Using a library function

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,20 +1,18 @@
 # pcost.py
 #
 # Exercise 1.2
+import csv
 
 def portfolio_cost(filepath):
   total_cost = 0
   with open(filepath, 'rt') as f:
-    headers = next(f).strip().split(',')
-    #print(headers)
-    for line in f:
-      row = line.strip().split(',')
-      #print(row)
+    rows = csv.reader(f)
+    headers = next(rows)
+    for row in rows:
       try:
         total_cost += int(row[1]) * float(row[2])
       except ValueError:
-        print("Couldn't parse", repr(line))
-
+        print("Couldn't parse", repr(row))
   return total_cost
 
 cost = portfolio_cost('Data/portfolio.csv')
