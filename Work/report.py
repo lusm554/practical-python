e2e73f88e53a4a7336c6028c5cf72957d3e5f263
lusm <loveyousomuch554@gmail.com>
2023-07-21 21:22:06 +0300

    Exercise 2.6: Dictionaries as a container

--- Work/report.py
+++ Work/report.py
@@ -19,6 +19,21 @@ def read_portfolio(filename):
       portfolio.append(curr_share)
   return portfolio
 
+def read_prices(filename):
+  prices = {}
+  with open(filename, 'rt') as f:
+    rows = csv.reader(f)
+    header = next(rows)
+    for row in rows:
+      try:
+        name, price = row
+        prices[name] = price
+      except ValueError:
+        print('Couldn\'t parse', row)
+  return prices
 
-test = read_portfolio('Data/portfolio.csv')
+test = read_prices('Data/prices.csv')
 pprint(test)
+
+#test = read_portfolio('Data/portfolio.csv')
+#pprint(test)
