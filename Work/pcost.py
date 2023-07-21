db0130fa53c2f821cf0141e86b03cc48bbfb3f8d
lusm <loveyousomuch554@gmail.com>
2023-07-21 14:36:01 +0300

    Exercise 1.30: Turning a script into a function

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,15 +2,16 @@
 #
 # Exercise 1.2
 
-filepath = 'Data/portfolio.csv'
-total_cost = 0.0
+def portfolio_cost(filepath):
+  total_cost = 0
+  with open(filepath, 'rt') as f:
+    headers = next(f).strip().split(',')
+    #print(headers)
+    for line in f:
+      row = line.strip().split(',')
+      #print(row)
+      total_cost += int(row[1]) * float(row[2])
+  return total_cost
 
-with open(filepath, 'rt') as f:
-  headers = next(f).strip().split(',')
-  print(headers)
-  for line in f:
-    row = line.strip().split(',')
-    print(row)
-    total_cost += int(row[1]) * float(row[2])
-
-print(f"{total_cost=}")
+cost = portfolio_cost('Data/portfolio.csv')
+print(f"{cost=}")
