9d09c1097a5fdfcb66559aad727bc180b342313e
lusm <loveyousomuch554@gmail.com>
2023-07-21 21:13:35 +0300

    Exercise 2.5: List of Dictionaries

--- Work/report.py
+++ Work/report.py
@@ -2,6 +2,7 @@
 #
 # Exercise 2.4
 import csv
+from pprint import pprint
 
 def read_portfolio(filename):
   portfolio = []
@@ -9,6 +10,15 @@ def read_portfolio(filename):
     rows = csv.reader(f)
     header = next(rows)
     for row in rows:
-      curr_share = (row[0], int(row[1]), float(row[2]))
+      #curr_share = (row[0], int(row[1]), float(row[2]))
+      curr_share = {
+        'name': row[0],
+        'shares': int(row[1]),
+        'price': float(row[2]),
+      }
       portfolio.append(curr_share)
   return portfolio
+
+
+test = read_portfolio('Data/portfolio.csv')
+pprint(test)
