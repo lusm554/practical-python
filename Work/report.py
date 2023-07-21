25825e8f44bff92b960e1243ef37c97128769f0b
lusm <loveyousomuch554@gmail.com>
2023-07-21 21:07:04 +0300

    Exercise 2.4: A list of tuples

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,14 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+  portfolio = []
+  with open(filename, 'rt') as f:
+    rows = csv.reader(f)
+    header = next(rows)
+    for row in rows:
+      curr_share = (row[0], int(row[1]), float(row[2]))
+      portfolio.append(curr_share)
+  return portfolio
