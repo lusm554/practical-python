d8d53490e0b07fdacfb506f70233f667a41ae5fc
lusm <loveyousomuch554@gmail.com>
2023-07-26 23:04:37 +0300

    Exercise 3.3: Reading CSV Files

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,18 @@
 # fileparse.py
 #
 # Exercise 3.3
+import csv
+
+def parse_csv(filename: str) -> list[dict]:
+  """
+  Parse a CSV files into a list of records.
+  """
+  with open(filename, 'rt') as f:
+    rows = csv.reader(f)
+    header = next(rows)
+    records = []
+    for row in rows:
+      if not row: continue # skip empty rows
+      record = dict(zip(header, row))
+      records.append(record)
+  return records
