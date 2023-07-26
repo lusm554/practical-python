0e61acf317b037d1dfe81c86f771c30347f4a73b
lusm <loveyousomuch554@gmail.com>
2023-07-26 23:21:35 +0300

    Exercise 3.5: Performing Type Conversion

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -3,7 +3,7 @@
 # Exercise 3.3
 import csv
 
-def parse_csv(filename: str, select: bool=False) -> list[dict]:
+def parse_csv(filename: str, select: list = None, types: list = None) -> list[dict]:
   """
   Parse a CSV files into a list of records.
   """
@@ -12,6 +12,8 @@ def parse_csv(filename: str, select: bool=False) -> list[dict]:
     header = next(rows)
     if select:
       indices = [header.index(colname) for colname in select]
+      if types:
+        types = [types[index] for index in indices]
     else:
       indices = []
     records = []
@@ -19,6 +21,23 @@ def parse_csv(filename: str, select: bool=False) -> list[dict]:
       if not row: continue # skip empty rows
       if indices:
         row = [row[index] for index in indices]
+      if types:
+        row = [cast(val) for val, cast in zip(row, types)]
       record = dict(zip(header, row))
       records.append(record)
   return records
+
+# TEST CASE
+from pprint import pprint
+p = parse_csv('Data/portfolio.csv')
+pprint(p)
+
+p = parse_csv('Data/portfolio.csv', select=['name', 'price'])
+pprint(p)
+
+p = parse_csv('Data/portfolio.csv', select=['name', 'price'], types=[str, int, float])
+pprint(p)
+
+p = parse_csv('Data/portfolio.csv', types=[str, int, float])
+pprint(p)
+
