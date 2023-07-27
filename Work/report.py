44bebbeb86c8c43343deae2e7d73df86cb80ec76
lusm <loveyousomuch554@gmail.com>
2023-07-27 22:58:28 +0300

    Exercise 4.5: An Extensibility Problem

--- Work/report.py
+++ Work/report.py
@@ -4,16 +4,16 @@
 from pprint import pprint
 import fileparse
 import stock
+import tableformat
+
 
 def read_portfolio(filename):
   portfolio = [ stock.Stock(*p.values()) for p in fileparse.parse_csv(filename, types=[str, int, float]) ]
   return portfolio
 
-
 def read_prices(filename):
   prices = dict(fileparse.parse_csv(filename, types=[str, float], has_headers=False))
   return prices
-
 
 def make_report(portfolio, prices):
   report = []
@@ -25,19 +25,24 @@ def make_report(portfolio, prices):
     report.append(row)
   return report
 
-def print_report(report):
-  headers = ('Name', 'Shares', 'Price', 'Change')
+def print_report(report, formatter):
+  formatter.headings(('Name', 'Shares', 'Price', 'Change'))
+  """
   separator = ('-' * 10,) * 4
   print('%10s %10s %10s %10s' % headers)
   print('%s %s %s %s' % separator)
+  """
   for name, shares, price, change in report:
-    print('%10s %10d %10s %10.2f' % (name, shares, ("$%.2f" % price), change))
+    #print('%10s %10d %10s %10.2f' % (name, shares, ("$%.2f" % price), change))
+    rowdata = [ name, str(shares), f'{price:0.2f}', f'{change:0.2f}' ]
+    formatter.row(rowdata)
 
 def portfolio_report(portfoliofile, pricesfile):
   portfolio = read_portfolio(portfoliofile)
   prices = read_prices(pricesfile)
   report = make_report(portfolio, prices)
-  print_report(report)
+  formatter = tableformat.TableFormatter()
+  print_report(report, formatter)
 
 def main(argv):
   #portfolio_report('Data/portfolio.csv', 'Data/prices.csv')
