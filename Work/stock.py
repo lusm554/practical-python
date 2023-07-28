6d73e1b669aa76e016cf89878ba7da3516c0ee08
lusm <loveyousomuch554@gmail.com>
2023-07-28 18:56:00 +0300

    Exercise 4.9: Better output for printing objects

--- Work/stock.py
+++ Work/stock.py
@@ -4,6 +4,9 @@ class Stock:
     self.shares = shares
     self.price = price
 
+  def __repr__(self):
+    return f"{type(self).__name__}('{self.name}', {self.shares}, {self.price})"
+
   def cost(self):
     return self.shares * self.price
 
