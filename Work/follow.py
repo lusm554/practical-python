e85ac88086b1257b377946a5f61d1d07c66d470d
lusm <loveyousomuch554@gmail.com>
2023-07-29 19:53:06 +0300

    Exercise 6.6: Using a generator to produce data

--- Work/follow.py
+++ Work/follow.py
@@ -1,9 +1,28 @@
 import os
 import time
 
+
 f = open("Data/stocklog.csv")
 f.seek(0, os.SEEK_END)
 
+
+def follow(filepath):
+  with open(filepath) as f:
+    f.seek(0, os.SEEK_END)
+    while True:
+      line = f.readline()
+      if line == '':
+        time.sleep(.1)
+        continue
+      yield line
+
+if __name__ == '__main__':
+  for line in follow('Data/stocklog.csv'):
+    fields = line.split(',')
+    name, price, change = fields[0].strip(), float(fields[1]), float(fields[4])
+    if change < 0:
+      print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+"""
 while True:
   line = f.readline()
   if line == '':
@@ -13,3 +32,5 @@ while True:
   name, price, change = fields[0].strip(), float(fields[1]), float(fields[4])
   if change < 0:
     print(f'{name:>10s} {price:>10.2f} {change:>10.2f}')
+
+"""
