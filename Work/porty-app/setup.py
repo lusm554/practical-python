cdef5ddd94877710a9340b138b5beca3daa912d1
lusm <loveyousomuch554@gmail.com>
2023-07-30 19:55:20 +0300

    Exercise 9.5: Make a package

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,11 @@
+# setup.py
+import setuptools
+
+setuptools.setup(
+  name='porty',
+  version='0.0.1',
+  author='lusm554',
+  author_email='loveyousomuch554@gmail.com',
+  description='Practical Python Code',
+  packages=setuptools.find_packages(),
+)
